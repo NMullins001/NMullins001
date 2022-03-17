9c61ade20cc26ab8f9c69239b09729daf2e08b9a
Alexander Stapleman <StaplemanA001@mymail.mssu.edu>
2022-03-16 20:46:34 -0500

    Test Commit

--- CarRentalUI/obj/Debug/net5.0-windows/App.g.cs
+++ CarRentalUI/obj/Debug/net5.0-windows/App.g.cs
@@ -45,7 +45,7 @@ namespace CarRentalUI {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "5.0.15.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.1.0")]
         public void InitializeComponent() {
             
             #line 5 "..\..\..\App.xaml"
@@ -60,7 +60,7 @@ namespace CarRentalUI {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "5.0.15.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.1.0")]
         public static void Main() {
             CarRentalUI.App app = new CarRentalUI.App();
             app.InitializeComponent();
